0081b845bdffe7a4ad2ae4d8c6814f4bc0c58629
asimgunduz <a.gunduz@live.com>
2022-10-11 10:03:52 +0300

    CITIES

---
+++ MudXComponents.Shared/CITIES.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MudXComponents.Shared;
+
+public class CITIES
+{
+    [Key]
+    public string CIT_CODE { get; set; }
+
+    public string CIT_COUNTRY_CODE { get; set; }
+
+    public string CIT_DESC { get; set; }
+}
